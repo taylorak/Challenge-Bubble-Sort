34ed12612b6f1001fb99e85d14997696f745b4d4
Taylor Kennedy <taykenned@gmail.com>
2016-03-29 11:52:17 -1000

    adds tests for bubbleSort

---
+++ test/bubbleSort.spec.js
@@ -0,0 +1,26 @@
+var bubbleSort = require('../bubbleSort');
+Array.prototype.bubbleSort = bubbleSort;
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Bubble Sort', function() {
+  var unsortedArray;
+  var sortedArray = [1, 2 , 3, 4, 5];
+
+  beforeEach(function() {
+    unsortedArray = [5, 4, 3 , 2, 1]
+  })
+
+  it('should be a method of the Array object', function() {
+    expect(Array.bubbleSort).to.be.a('function');
+  })
+
+  it('should take an input Array and apply the bubble sort', function() {
+    unsortedArray.bubbleSort();
+    expect(unsortedArray).to.deep.equal(sortedArray);
+  })
+
+  it('should return the number of moves that were necessary to sort Array', function() {
+    expect(unsortedArray.bubbleSort()).to.equal(10);
+  })
+})
