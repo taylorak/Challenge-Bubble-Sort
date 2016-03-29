cbf464deded4cfa16dbe9db88ff294c118a84481
Taylor Kennedy <taykenned@gmail.com>
2016-03-29 12:25:22 -1000

    improves bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,20 +1,39 @@
-bubbleSort = function() {
+function bubbleSort() {
   var isSorted = false;
   var count = 0;
+  var arrlength = this.length - 1;
 
   while(!isSorted) {
     var isSorted = true;
-    for(var i = 0; i < this.length - 1; i++) {
+    for(var i = 0; i < arrlength; i++) {
       if(this[i + 1] < this[i]) {
-        var tmp = this[i + 1];
-        this[i + 1] = this[i];
-        this[i] = tmp;
+        swap(this, i, i + 1);
         var isSorted = false;
+        count++;
+      }
+    }
+    arrlength--;
+  }
+  return count;
+}
+
+function bubbleSort2() {
+  var count = 0;
+  for(var i = this.length - 1; i >= 0; i--) {
+    for(var j = 0; j <= i; j++) {
+      if(this[j + 1] < this[j]) {
+        swap(this, j, j + 1)
         count++;
       }
     }
   }
   return count;
+}
+
+function swap(arr, firstIndex, secondIndex) {
+  var tmp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = tmp;
 }
 
 module.exports = bubbleSort;
